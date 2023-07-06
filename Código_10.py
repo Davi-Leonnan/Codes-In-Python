d72507bb401650f88d153211406e3d256c8e3069
Leon_Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2023-07-06 14:39:54 -0300

    Add files via upload

---
+++ Código_10.py
@@ -0,0 +1,19 @@
+# Matemática Aplicada:
+
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+n = 5 + (4 * 10)
+
+print(round(pi))  #Arredonda o número.
+print(math.ceil(pi))  #Arredonda o número para o número seguinte.
+print(math.floor(pi))  #Arredonda o número para o número anterior.
+print(abs(y))  # Replica o número.
+print(pow(pi, 2))  # potencializa o número em 2.
+print(math.sqrt(4))  # Extrai a raiz do número.
+print(max(x, y, z, n))  # Mostra a variável de maior valor.
+print(min(x, y, z, n))  # Mostra a variável de menor valor.
