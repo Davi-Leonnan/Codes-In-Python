bed80903feb639340895e852470a52017725e126
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2024-07-24 10:26:12 -0300

    Add files via upload

---
+++ Python do Zero/25 -  Funções.py
@@ -0,0 +1,16 @@
+# Funções -  Chamar uma função somente quando é solicitada:
+
+
+# Caso queira adiconar mais de um nome, defina dois parâmetros dentro do parentese.
+
+def hello(first_name, last_name, age):  # Ao querer adicionar um valor de entrada para a função, defina o nome do parâmetro dentro do parentese e o adioione na função "print".
+    print("hello " + first_name + " " + last_name)
+    print("You are " + str(age) + " years old")  # Uma string de número sempre irá requerer a função "str".
+    print("Have a nice day!")  # Uma função sempre começa com "def" e requer parenteses no nome da função.
+
+# my_name = "Davi"
+
+hello("Ednaldo", "Pereira", 51)
+
+
+
---
+++ Python do Zero/26 - Declaração de Retorno.py
@@ -0,0 +1,8 @@
+# Declaração de Retorno - A função de uma ação retorna um certo valor:
+
+def multiply(number_01, number_02):
+    return number_01 * number_02
+
+x = (multiply(6,8))
+
+print(x)
