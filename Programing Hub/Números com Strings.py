229e8530dda52ef8b4f0426d8dfa714fe201f3ff
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-03-22 14:05:13 -0300

    Add files via upload

---
+++ Programing Hub/Números com Strings.py
@@ -0,0 +1,52 @@
+# Números com Strings:
+
+# O "import math" será necessário nessa sessão.
+
+import math
+
+# Oferece o exponencial do primeiro número:
+#math.pow(4,3)
+
+
+# Oferece o valor exato de um número:
+
+# Exemplo 1:
+
+a = math.floor(4.3)
+b = math.floor(10.9)
+print(a)
+print(b)
+
+# Exemplo 2:
+
+c = math.floor(55.75)
+d = math.floor(500.2475)
+print(c)
+print(d)
+
+
+# Oferece o valor exato aproximado de um número:
+
+# Exemplo 1:
+
+e = math.ceil(4.5)
+f = math.ceil(25.75)
+print(e)
+print(f)
+
+
+# Oferece o logaritmo do primeiro número em relação ao segundo:
+
+g = math.log(10)
+h = math.log(10,2)
+print(g)
+print(h)
+
+
+# Oferece a raiz quadrada de um número:
+
+i = math.sqrt(9)
+j = math.sqrt(64)
+print(i)
+print(j)
+
---
+++ Programing Hub/Strings e suas Funções.py
@@ -0,0 +1,25 @@
+# Strings e sua Funções:
+
+
+# str.capitalize(): Oferece a reaplicação de um texto inserido.
+
+# Exemplo 1:
+
+str = "I'm the coolest person in the world!"
+str.capitalize()
+
+
+# str.count(): Retorna a quantidade de letras em uma frase.
+
+# Exemplo 1:
+
+str = "Raise and shine, Mr.Freeman."
+str.count("e")
+
+
+# str.join(): Oferece a junção de direferentes palavras separadadas.
+
+str = " "
+item = ("I", "feel", "fantastic", "ei ei ei")
+a = str.join(item)
+print(a)
