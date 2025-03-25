556e6034462b2a264d0a3f95c63b75b381871d28
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-03-25 13:57:24 -0300

    Add files via upload

--- Programing Hub/Funções em Python.py
+++ Programing Hub/Funções em Python.py
@@ -1,5 +1,6 @@
 # Funções em Python:
 
+
 # Exemplo 01:
 
 def helloWorld():
@@ -7,4 +8,48 @@ def helloWorld():
     return
 
 helloWorld()
+
+
+# Exemplo 2:
+
+# Soma de números:
+
+def addNumbers(num1, num2):
+    sum = num1 + num2
+    print(sum)
+    return
+
+addNumbers(2,3)
+addNumbers(5,4)
+
+# Subtração de numeros:
+
+def minNumbers(num1, num2):
+    mim = num1 - num2
+    print(mim)
+    return
+
+minNumbers(10,5)
+minNumbers(50, 25)
+
+# Multiplicaçaõ de numeros:
+
+def MulNumbers(num1, num2):
+    Mul = num1 * num2
+    print(Mul)
+    return
+
+MulNumbers(2,2)
+MulNumbers(25,4)
+
+# Divisão de numeros:
+
+def divNumbers(num1, num2):
+    Div = num1 / num2
+    print(Div)
+    return
+
+divNumbers(9,3)
+divNumbers(1000,3)
+
 
