90fb34bca800dff2bf075d3942461872f49d2b44
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-11-17 23:17:06 -0300

    Add files via upload

---
+++ Mimo/Condicionais.py
@@ -0,0 +1,19 @@
+Age = 20
+
+if Age == 20:
+    print("You are able to drive.")
+else:
+    print("You are not able to drive.")
+    
+character = "warrior"
+
+if character == "wizard":
+    print("You can use the posion")
+else:
+    print("You can not use the posion.")
+    
+person = "king"
+power = "sword"
+
+if person == "king":
+    print(f"You can use the {power}")
---
+++ Mimo/Condicional_While.py
@@ -0,0 +1,8 @@
+Is_On = "Maria"
+
+while Is_On == "Maria":
+    print(Is_On)
+    Is_On = False
+    
+print("James")
+
