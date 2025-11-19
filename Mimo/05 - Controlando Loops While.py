a4ddf71fae389560ad53ad87c6b3e4be48988ebb
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-11-19 09:28:22 -0300

    Add files via upload

---
+++ Mimo/05 - Controlando Loops While.py
@@ -0,0 +1,12 @@
+Counter = 10
+
+while Counter <= 20:
+    print(Counter)
+    Counter += 1
+
+Loop = "The cake is a lie"
+
+contador_repeticoes = 0
+while contador_repeticoes < 10:
+    print(Loop) # Imprime o valor da variável "Loop"
+    contador_repeticoes += 1 # Incrementa o contador
