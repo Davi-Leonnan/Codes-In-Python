dacb25e1a4aca9364d901338bc0dc79fe4229e97
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-11-21 08:59:28 -0300

    Add files via upload

---
+++ Mimo/Loops_For.py
@@ -0,0 +1,22 @@
+# A função "loops for" cria loops muitos mais dinâmicos e fáceis de entender.
+
+# A variável "in" (i) é a variável contadora, a qual mostra em qual posição a contagem está no momento.
+
+# Exemplo 01:
+
+for i in range(5):
+    print(i)
+    print("The cake is a lie")
+
+for i in range(5):
+    print(i)
+    print("Don't trust in Glados!")
+
+# exemplo 02:
+
+print("Get ready to the race!")
+
+for x in range(11):
+    print(x)
+
+print("RUN!")
