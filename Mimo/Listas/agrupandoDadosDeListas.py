58bddad73c745e88e69c80d0cefff7d727218564
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-11-26 15:50:14 -0300

    Add files via upload

---
+++ Mimo/Listas/agrupandoDadosDeListas.py
@@ -0,0 +1,7 @@
+# Podemos adicionar dados dentro de uma lista colocando-os entre colchetes. "[]".
+
+# Os valores armazenados na lista são chamados de "elementos".
+
+Fruits = ["Apple", "Orange", "Grape", "Banana", "Melon", "Pear"]
+
+print(Fruits)
---
+++ Mimo/Listas/alterandoDadosEmListas.py
@@ -0,0 +1,18 @@
+# Cada item da item tem uma posição numerada chamada de "índice".
+
+# Os índices começam do "0" e vão até a última posição em que o último elemento da lista se encontra.
+
+# Exemplo:
+
+left4DeadCharacters = ['Bill', 'Francis', 'Zoey', 'Louie']
+
+print(left4DeadCharacters[2])
+
+
+left4Dead2Characters = ["Coach", "Nick", "Ellis", "Rochelle"]
+
+left4Dead2Characters[2] = "Gnomo"
+
+print(left4Dead2Characters)
+
+
