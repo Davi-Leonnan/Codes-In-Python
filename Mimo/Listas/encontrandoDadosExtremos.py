f3d4e945bf7f3aa82c12db51fa6a903bf6098f4f
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-12-05 18:58:48 -0300

    Add files via upload

---
+++ Mimo/Listas/encontrandoDadosExtremos.py
@@ -0,0 +1,26 @@
+# Para encontrar o maior valor em uma lista, utilizamos a função "max" como o nome da lista entre parenteses.
+
+# Exemplo 01:
+
+grades = [9.5, 8.7, 4.5, 7.6, 6.8, 9.8]
+
+print(max(grades))
+
+
+# Para encontrar o menor valor em uma lista, utilizamos a função "min" como o nome da lista entre parenteses.
+
+# Exemplo 02:
+
+Montantes = [14000, 12000, 13500, 8000, 17800, 20050,]
+
+print(min(Montantes))
+
+# Exemplo 03:
+
+Amostras = [2.1, 4.3, 5.3, 3.7, 7.2, 3.9]
+
+Menor_Amostra = min(Amostras)
+
+print(Menor_Amostra)
+
+
---
+++ Mimo/Listas/ordenandoDados.py
@@ -0,0 +1,17 @@
+# Caso queiramos ordenar valores de uma lista do menor pro maior, utilizamos a função ".sort()".
+
+# Exemplo 01:
+
+resultados = [7, 8, 4.5, 9.7, 7.5, 5, 6, 8.5, 10, 9, 6.8]
+
+resultados.sort()
+
+print(resultados)
+
+# Exemplo 02:
+
+gods = ["Zeus", "Poseidon", "Hades", "Ares", "Hera", "Dermete", "Athena", "Hermes", "Apollo", "Artemis", "Herfesto"]
+
+gods.sort()
+
+print(gods)
