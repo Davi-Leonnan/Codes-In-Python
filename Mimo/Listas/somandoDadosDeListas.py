ef11a07979099007d1249913dd155172b4c5dc5c
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-12-08 09:16:21 -0300

    Add files via upload

---
+++ Mimo/Listas/somandoDadosDeListas.py
@@ -0,0 +1,19 @@
+# Caso desejamos somar os dados de uma listas, uamos a função "sum()" entre parenteses.
+
+# Exemplo 01:
+
+Lucros = [10000, 15000, 17500, 18000, 14700, 11000, 125000]
+
+Lucro_Total = sum(Lucros)
+
+print(Lucro_Total)
+
+
+# Exemplo 02:
+
+Convidados = ["Maria", "Rafael", "Luiz", "Rafaela", "Sueli", "Renata", "Gabriel"]
+
+Participantes = len(Convidados)
+
+
+print(Convidados)
