f8079f7c279afae40f60675801a0412b6abf3605
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-12-09 09:31:20 -0300

    Add files via upload

---
+++ Mimo/Funções/01 - reutilizandoCodigosComFunçoes.py
@@ -0,0 +1,24 @@
+# Em python, incorporamos uma função utilizando a palavra-chave "def" seguida de parenteses, em seguida definimos o código relacionado a função.
+# Para chamar a função, reescrevemos a mesma após o bloco de código.
+
+
+# Exemplo 01:
+
+def saudaçoes():
+    print("Olá!")
+    print("Tudo bem?")
+
+saudaçoes()
+
+# Exemplo 02:
+
+valor_01 = 5
+
+valor_02 = 10
+
+def soma():
+    resultado = valor_01 + valor_02
+    print(resultado)
+
+soma()
+
