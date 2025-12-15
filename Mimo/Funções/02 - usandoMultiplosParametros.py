90462579e6c9f6c4aa07ec7a38b4cbeb0b168c50
Leon Silva <124218271+Davi-Leonnan@users.noreply.github.com>
2025-12-15 09:16:10 -0300

    Add files via upload

---
+++ Mimo/Funções/02 - usandoMultiplosParametros.py
@@ -0,0 +1,24 @@
+# Quando desejamos adicionar mais de um parâmetro em uma função, adicionamos uma vírgula 
+# após o primeiro parâmetro e em seguida adicionamos o segundo.
+
+# Exemplo 01:
+
+def nome(primeiro, segundo):
+    print(primeiro, "", segundo)
+
+nome("Adam", "First")
+
+
+# Exemplo 02: 
+
+def soverte(sabor_01, sabor_02):
+    print("Soverte de " + sabor_01 + " " + "Com " + sabor_02)
+
+soverte("Chocolate", "Morango")
+
+# Exemplo 03:
+
+def animals(animal_01, animal_02, animal_03, animal_04):
+    print(animal_01, animal_02, animal_03, animal_04)
+
+animals("Alex", "Martin", "Nelman", "Gloria")
